d43776122145055f1b2481ef8883f58607956081
naliwka <avierina.nataliia@gmail.com>
2023-03-12 01:15:52 +0200

    Lab_2-3 is done

---
+++ Lab_2-3/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_3
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+    }
+}
--- Lab_2-3/Program.cs
+++ Lab_2-3/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab_2_3
 {
@@ -10,6 +9,29 @@ namespace Lab_2_3
     {
         static void Main(string[] args)
         {
+            // Зчитування даних з файлу
+            List<Product> products = File.ReadAllLines("products.txt")
+                                          .Skip(1) // пропустити перший рядок (заголовок)
+                                          .Select(line => line.Split(';'))
+                                          .Select(parts => new Product
+                                          {
+                                              Name = parts[0],
+                                              Category = parts[1],
+                                              Price = Convert.ToDouble(parts[2])
+                                          })
+                                          .ToList();
+
+            var groupedProducts = products.GroupBy(p => p.Category);
+
+            foreach (var group in groupedProducts)
+            {
+                Console.WriteLine(group.Key + ":");
+                foreach (var product in group)
+                {
+                    Console.WriteLine($" {product.Name} - {product.Price}");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
