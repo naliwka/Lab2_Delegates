b33afd3520c51196b99058d500edcd3366591b00
naliwka <avierina.nataliia@gmail.com>
2023-03-11 19:46:43 +0200

    Lab_2-1 is done

--- Lab_2-1/Program.cs
+++ Lab_2-1/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab_2_1
 {
@@ -10,6 +8,22 @@ namespace Lab_2_1
     {
         static void Main(string[] args)
         {
+            // Add students to the list
+            List<Student> students = new List<Student>()
+            {
+                new Student { Name = "Samantha", Grade = 66 },
+                new Student { Name = "Linda", Grade = 98 },
+                new Student { Name = "John", Grade = 75 },
+                new Student { Name = "Kate", Grade = 91 },
+                new Student { Name = "Tom", Grade = 90 }
+            };
+            // Filter out students with a score greater than 85
+            List<Student> filteredStudents = students.Where(s => s.Grade > 85).ToList();
+            // Display the list of filtered students
+            foreach (Student student in filteredStudents)
+            {
+                Console.WriteLine($"{student.Name} - {student.Grade}");
+            }
         }
     }
 }
---
+++ Lab_2-1/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Lab_2_1
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+    }
+}
