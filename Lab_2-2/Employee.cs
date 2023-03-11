e4ae7a316193c1198d0918ef517a137cb755db48
naliwka <avierina.nataliia@gmail.com>
2023-03-11 21:08:16 +0200

    Lab_2-2 is done

---
+++ Lab_2-2/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Lab_2_2
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+}
--- Lab_2-2/Program.cs
+++ Lab_2-2/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Lab_2_2
 {
@@ -10,6 +9,23 @@ namespace Lab_2_2
     {
         static void Main(string[] args)
         {
+            List<Employee> employees = new List<Employee>();
+
+            using (StreamReader reader = new StreamReader("employee.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] parts = line.Split(' ');
+                    employees.Add(new Employee { Name = parts[0], Salary = int.Parse(parts[1])});
+                } 
+            }
+            List<Employee> sortedEmployees = employees.OrderBy(e => e.Salary).ToList();
+
+            foreach (Employee employee in sortedEmployees)
+            {
+                Console.WriteLine($"{employee.Name} - {employee.Salary}");
+            }
         }
     }
 }
